e8f5f56206efdd3f572fff3c2ec9102e62a2b87d
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-02-03 23:45:21 +0900

    Add section6

--- Concurrency-in-Go/chapter4_Patterns_of_Go_Concurrency/section6_pipeline/bench_test.go
+++ Concurrency-in-Go/chapter4_Patterns_of_Go_Concurrency/section6_pipeline/bench_test.go
@@ -4,11 +4,59 @@ import (
 	"testing"
 )
 
+// $ go test -bench .
+// goos: darwin
+// goarch: amd64
+// BenchmarkGeneric-4        869703              1280 ns/op
+// BenchmarkTyped-4         1479144               839 ns/op
+
+// > interface{}をStringにする処理をした方が1.5倍ほど処理がかかるが単位で言えばごく小さな差です。
+
 func BenchmarkGeneric(b *testing.B) {
 	done := make(chan interface{})
 	defer close(done)
 
 	b.ResetTimer()
 	for range toString(done, take(done, repeat(done, "a"), b.N)) {
+	}
+}
+
+func BenchmarkTyped(b *testing.B) {
+	repeat := func(done <-chan interface{}, values ...string) <-chan string {
+		valueCh := make(chan string)
+		go func() {
+			defer close(valueCh)
+			for {
+				for _, v := range values {
+					select {
+					case <-done:
+						return
+					case valueCh <- v:
+					}
+				}
+			}
+		}()
+		return valueCh
+	}
+	take := func(done <-chan interface{}, valueCh <-chan string, num int) <-chan string {
+		takeCh := make(chan string)
+		go func() {
+			defer close(takeCh)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeCh <- <-valueCh:
+				}
+			}
+		}()
+		return takeCh
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	b.ResetTimer()
+	for range take(done, repeat(done, "a"), b.N) {
 	}
 }
