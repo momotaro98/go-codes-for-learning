d15e4fdf5eed213c339f44507d44c88eca820645
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-02-04 14:08:48 +0900

    Update bench

--- go-comparison/slice_append_vs_assign/bench_code_test.go
+++ go-comparison/slice_append_vs_assign/bench_code_test.go
@@ -1,30 +1,58 @@
 package main
 
-import "testing"
-
-// BenchmarkAppendNakedMap-4        6103308               187 ns/op             248 B/op          5 allocs/op
-// BenchmarkAssignMap-4            20554850                52.3 ns/op            80 B/op          1 allocs/op
-// BenchmarkAppendCapMap-4         20464335                56.6 ns/op            80 B/op          1 allocs/op
-
-var (
-	s = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	l = len(s)
-	f = func(i int) int { return i * 2 }
+import (
+	"testing"
 )
 
+var (
+	sl = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	ln = len(sl)
+	fn = func(i int) int { return i * 2 }
+)
+
+func AppendNakedMap(in []int, length int, f func(i int) int) []int {
+	var ret []int
+	for _, c := range in {
+		ret = append(ret, f(c))
+	}
+	return ret
+}
+
 func BenchmarkAppendNakedMap(b *testing.B) {
+	s, l, f := sl, ln, fn
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = AppendNakedMap(s, l, f)
 	}
 }
 
+func AssignMap(in []int, length int, f func(i int) int) []int {
+	ret := make([]int, length)
+	for i, c := range in {
+		ret[i] = f(c)
+	}
+	return ret
+}
+
 func BenchmarkAssignMap(b *testing.B) {
+	s, l, f := sl, ln, fn
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = AssignMap(s, l, f)
 	}
 }
 
+func AppendCapMap(in []int, length int, f func(i int) int) []int {
+	ret := make([]int, 0, length)
+	for _, c := range in {
+		ret = append(ret, f(c))
+	}
+	return ret
+}
+
 func BenchmarkAppendCapMap(b *testing.B) {
+	s, l, f := sl, ln, fn
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_ = AppendCapMap(s, l, f)
 	}
