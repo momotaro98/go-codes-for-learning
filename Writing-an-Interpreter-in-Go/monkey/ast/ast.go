c935d427725357961742c2fb32bc2287d13db542
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-05-12 22:18:56 +0900

    Add AST

---
+++ Writing-an-Interpreter-in-Go/monkey/ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import (
+	"github.com/momotaro98/go-codes-for-learning/Writing-an-Interpreter-in-Go/monkey/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET token
+	Name  *Identifier // 変数名
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
