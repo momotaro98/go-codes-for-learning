a2e788ac9c7a5f929f53a1ea428e917ea7da6629
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-06-05 23:55:21 +0900

    3.5.2 Finishing REPL

--- Writing-an-Interpreter-in-Go/monkey/repl/repl.go
+++ Writing-an-Interpreter-in-Go/monkey/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/momotaro98/go-codes-for-learning/Writing-an-Interpreter-in-Go/monkey/evaluator"
 	"github.com/momotaro98/go-codes-for-learning/Writing-an-Interpreter-in-Go/monkey/lexer"
 	"github.com/momotaro98/go-codes-for-learning/Writing-an-Interpreter-in-Go/monkey/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
