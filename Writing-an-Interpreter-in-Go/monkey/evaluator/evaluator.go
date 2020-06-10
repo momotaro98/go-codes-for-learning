5eb12e885932a32d67a5b808a30d08d1cb7c2760
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-06-10 23:04:14 +0900

    3.6 If statement 条件分岐

--- Writing-an-Interpreter-in-Go/monkey/evaluator/evaluator.go
+++ Writing-an-Interpreter-in-Go/monkey/evaluator/evaluator.go
@@ -18,6 +18,12 @@ func Eval(node ast.Node) object.Object {
 	case *ast.Program:
 		return evalStatements(node.Statements)
 
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
+
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 
@@ -131,5 +137,30 @@ func evalIntegerInfixExpression(operator string,
 		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
