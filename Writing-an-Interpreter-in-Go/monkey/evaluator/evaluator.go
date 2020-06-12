e308f1e88b44b48e9945cabae353c0079a014bed
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-06-12 22:25:35 +0900

    3.7 return statement 2nd

--- Writing-an-Interpreter-in-Go/monkey/evaluator/evaluator.go
+++ Writing-an-Interpreter-in-Go/monkey/evaluator/evaluator.go
@@ -16,10 +16,10 @@ func Eval(node ast.Node) object.Object {
 
 	// Statements
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node)
 
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatement(node)
 
 	case *ast.IfExpression:
 		return evalIfExpression(node)
@@ -51,14 +51,33 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
-	for _, statement := range stmts {
+	for _, statement := range program.Statements {
 		result = Eval(statement)
 
 		if returnValue, ok := result.(*object.ReturnValue); ok {
 			return returnValue.Value
+		}
+	}
+
+	return result
+}
+
+func evalBlockStatement(
+	block *ast.BlockStatement,
+) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil {
+			rt := result.Type()
+			if rt == object.RETURN_VALUE_OBJ {
+				return result
+			}
 		}
 	}
 
