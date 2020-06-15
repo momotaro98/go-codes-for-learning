5f421ee326d59492558049dca1826415f30a4731
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-06-15 23:48:58 +0900

    Try pkg/errors WithStack func

---
+++ official-packages/pkg-errors/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+func repo() error {
+	cause := errors.New("core cause of error like DB command execution error")
+	return errors.WithStack(cause)
+}
+
+func service() error {
+	return repo()
+}
+
+func main() {
+	fmt.Printf("%+v", service())
+}
