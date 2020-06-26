5a1d2fc55cffe0f1e4e37f222b32161a0b6fb8e9
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-06-26 23:19:00 +0900

    3.10 Function and Caller finished

--- Writing-an-Interpreter-in-Go/monkey/object/environment.go
+++ Writing-an-Interpreter-in-Go/monkey/object/environment.go
@@ -8,6 +8,12 @@ func NewEnclosedEnvironment(outer *Environment) *Environment {
 }
 */
 
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s, outer: nil}
@@ -20,11 +26,9 @@ type Environment struct {
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
-	/*
-		if !ok && e.outer != nil {
-			obj, ok = e.outer.Get(name)
-		}
-	*/
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
