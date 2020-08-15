2666c9cb06a41bc3f84b7898d5facb4ec66f57ec
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-08-15 21:33:53 +0900

    before adding new relic

---
+++ 3rd-parties/newrelic/go-agent/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+
+	goji "goji.io"
+	"goji.io/pat"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func compServer(r *goji.Mux) {
+	r.HandleFunc(pat.Get("/comhash"), compHandler)
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
+
+type resCom struct {
+	OK bool `json:"ok"`
+}
+
+func compHandler(w http.ResponseWriter, r *http.Request) {
+	stored := r.URL.Query().Get("stored")
+	input := r.URL.Query().Get("input")
+
+	err := bcrypt.CompareHashAndPassword([]byte(stored), []byte(input))
+	if err == bcrypt.ErrMismatchedHashAndPassword {
+		// log.Println("got error in compare hash", err, "stored:", stored, "input:", input)
+		w.Header().Set("Content-Type", "application/json;charset=utf-8")
+		json.NewEncoder(w).Encode(resCom{OK: false})
+		return
+	}
+	if err != nil {
+		// log.Println("got error in compare hash", err, "stored:", stored, "input:", input)
+		w.Header().Set("Content-Type", "application/json;charset=utf-8")
+		json.NewEncoder(w).Encode(resCom{OK: false})
+		return
+	}
+
+	// log.Println("comp OK!", "stored:", stored, "input:", input)
+
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	json.NewEncoder(w).Encode(resCom{OK: true})
+	return
+}
+
+func main() {
+	mux := goji.NewMux()
+
+	go compServer(mux)
+
+	mux.HandleFunc(pat.Get("/sample"), sampleHandler)
+	log.Fatal(http.ListenAndServe(":8000", mux))
+}
+
+func sampleHandler(w http.ResponseWriter, r *http.Request) {
+	ok, err := conCompareHashAndPassword(r, "abc", "def")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
+
+func conCompareHashAndPassword(r *http.Request, stored, input string) (bool, error) {
+	stored = url.QueryEscape(stored)
+	input = url.QueryEscape(input)
+	url := fmt.Sprintf("http://localhost:5000/comhash?stored=%s&input=%s", stored, input)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return false, err
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("err in DefaultClient in conCompareHashAndPassword")
+		return false, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		log.Printf("status code: %d; body: %s\n", res.StatusCode, res.Body)
+		return false, fmt.Errorf("status code: %d; body: %s", res.StatusCode, res.Body)
+	}
+
+	type Response struct {
+		OK bool `json:"ok"`
+	}
+	ress := &Response{}
+	err = json.NewDecoder(res.Body).Decode(ress)
+	if err != nil {
+		log.Printf("decode failed in conCompareHashAndPassword")
+		return false, err
+	}
+	return ress.OK, nil
+}
