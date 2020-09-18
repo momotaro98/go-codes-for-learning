cdbdfe5966601868e3c570435eb12e35d74e2ed3
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-09-18 17:13:15 +0900

    fix gin

--- 3rd-parties/gin/main.go
+++ 3rd-parties/gin/main.go
@@ -1,49 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
+
+type MyStruct struct {
+	Key string `json:"key" binding:"required"`
+}
 
 func MyMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// before request
-		b := c.Request.Body
-		fmt.Println("in middleware", b)
+		var h MyStruct
+
+		if err := c.ShouldBindBodyWith(&h, binding.JSON); err != nil {
+			c.JSON(400, gin.H{
+				"message": fmt.Sprintf("%+v", err),
+			})
+			return
+		}
 
 		c.Next()
-
-		// after request
 	}
-}
-
-type Header struct {
-	Value string `json:"value" binding:"required"`
 }
 
 func main() {
 	r := gin.Default()
 	r.Use(MyMiddleware())
 	r.POST("/post", func(c *gin.Context) {
+		var h MyStruct
 
-		b := c.Request.Body
-		fmt.Println("in post", b)
-		defer b.Close()
+		// [My Note]
+		// このDecodeをする時点で 2重でデコードすると EOFが発生。
+		// この事象はginに依るところではない。
+		// c.BindJSON(&h) この中でjson.NewDecoderをしているのでEOFのエラーが発生していた。
+		// c.ShouldBindBodyWith を利用すれば c.Request.Body が消化されずに済む。
 
-		var h Header
-		// h.Value = "value"
-
-		if err := c.BindJSON(&h); err != nil {
-			fmt.Println(err)
+		decoder := json.NewDecoder(c.Request.Body)
+		err := decoder.Decode(&h)
+		if err != nil {
 			c.JSON(400, gin.H{
-				"message": fmt.Sprintf("err is %+v\n", err),
+				"message": fmt.Sprintf("%+v", err),
 			})
 			return
 		}
 
+		//if err := c.BindJSON(&h); err != nil {
+		//	fmt.Println(err)
+		//	c.JSON(400, gin.H{
+		//		"message": fmt.Sprintf("err is %+v\n", err),
+		//	})
+		//	return
+		//}
+
+		//if err := c.ShouldBindBodyWith(&h, binding.JSON); err != nil {
+		//	fmt.Println(err)
+		//	c.JSON(400, gin.H{
+		//		"message": fmt.Sprintf("err is %+v\n", err),
+		//	})
+		//	return
+		//}
+
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "ok",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
