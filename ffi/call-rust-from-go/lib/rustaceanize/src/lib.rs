4768e69eb4b9cfb7003470ffff690466bd085185
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-12-16 01:56:25 +0900

    fix

--- ffi/call-rust-from-go/lib/rustaceanize/src/lib.rs
+++ ffi/call-rust-from-go/lib/rustaceanize/src/lib.rs
@@ -1,10 +1,10 @@
-extern crate libc; // Cの機能があるパッケージ https://github.com/rust-lang/libc
+extern crate libc;
 use std::ffi::{CStr, CString};
 
-#[no_mangle] // Rustコンパイラが関数名を変えたり削除しないように必要
+#[no_mangle] // no_mangle はRustコンパイラが関数名を変えたり削除しないように必要
 pub extern "C" fn rustaceanize(name: *const libc::c_char) -> *const libc::c_char {
-    let buf_name = unsafe { CStr::from_ptr(name).to_bytes() }; // CStrはlibcの型を利用してCStr型を作っている
-    let mut str_name = String::from_utf8(buf_name.to_vec()).unwrap();
+    let cstr_name = unsafe { CStr::from_ptr(name) };
+    let mut str_name = cstr_name.to_str().unwrap().to_string();
     println!("Rustaceanizing \"{}\"", str_name);
     let r_string: &str = " (V)[0-0](V)";
     str_name.push_str(r_string);
