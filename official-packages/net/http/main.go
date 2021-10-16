3aaec5f5306020ca4a210ea583f7dd1ce5cb06ea
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2021-10-16 17:06:53 +0900

    update for net/http learning

--- official-packages/net/http/main.go
+++ official-packages/net/http/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+// HTTPサーバ
 func hello(w http.ResponseWriter, req *http.Request) {
-	// body ...
+	b1, _ := ioutil.ReadAll(req.Body) // この時点でbody.sawEOFがtrueになり次回はもう読み取れないようになっている
+	b2, _ := ioutil.ReadAll(req.Body) // 読み取れない
+	fmt.Println(b1)                   // [123 10 32 32 32 32 34 97 34 10 125]
+	fmt.Println(b2)                   // []
 
 	fmt.Fprintf(w, "hello\n")
 }
