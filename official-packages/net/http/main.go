1994a28542c98efcc6371c53b1b27ebaa0e7363e
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2021-12-03 23:43:32 +0900

    update

--- official-packages/net/http/main.go
+++ official-packages/net/http/main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-// HTTPサーバ
+// 失敗しないHTTPサーバ
 func hello(w http.ResponseWriter, req *http.Request) {
-	b1, _ := ioutil.ReadAll(req.Body) // この時点でbody.sawEOFがtrueになり次回はもう読み取れないようになっている
-	b2, _ := ioutil.ReadAll(req.Body) // 読み取れない
-	fmt.Println(b1)                   // [123 10 32 32 32 32 34 97 34 10 125]
-	fmt.Println(b2)                   // []
+	buf, _ := ioutil.ReadAll(req.Body)
+	bufForLog := bytes.NewBuffer(buf)
+	bufForApp := bytes.NewBuffer(buf)
+
+	fmt.Println(bufForLog)
+
+	req.Body = ioutil.NopCloser(bufForApp)
+	fmt.Println(req.Body)
 
 	fmt.Fprintf(w, "hello\n")
 }
+
+// // 失敗するHTTPサーバ
+// func hello(w http.ResponseWriter, req *http.Request) {
+// 	bufForLog, _ := ioutil.ReadAll(req.Body) // この時点でbody.sawEOFがtrueになり次回はもう読み取れないようになっている
+// 	fmt.Println(bufForLog)                   // [123 10 32 32 32 32 34 97 34 10 125]
+
+// 	bufForApp, _ := ioutil.ReadAll(req.Body) // すでに"消費"済みなので読み取れない
+// 	fmt.Println(bufForApp)                   // []
+
+// 	fmt.Fprintf(w, "hello\n")
+// }
 
 func main() {
 	http.HandleFunc("/hello", hello)
